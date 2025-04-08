5ae5e093646f31a23fa1a3ca5c34d0bdb141a20c
LOGIC <120298843+AAstraX@users.noreply.github.com>
2025-04-08 22:57:02 +0800

    Update adoptmescriptsts.lua

--- adoptmetb/adoptmescriptsts.lua
+++ adoptmetb/adoptmescriptsts.lua
@@ -1,9 +1,11 @@
-local function adoptme()
-        loadstring(game:HttpGet("https://arscripts.space/adoptmetb/AdoptmNS.lua"))()
-end
 local function bypassAC()
         loadstring(game:HttpGet("https://arscripts.space/adoptmetb/AdoptmeAC.lua"))()
 end
 
-task.spawn(adoptme)
+local function adoptme()
+        loadstring(game:HttpGet("https://arscripts.space/adoptmetb/AdoptmNS.lua"))()
+end
+
 task.spawn(bypassAC)
+task.spawn(adoptme)
+
