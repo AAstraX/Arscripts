6880753a3396b712d5631dd3ee614ab170b6568d
LOGIC <120298843+AAstraX@users.noreply.github.com>
2025-04-08 23:08:22 +0800

    Update adoptmescriptsts.lua

--- adoptmetb/adoptmescriptsts.lua
+++ adoptmetb/adoptmescriptsts.lua
@@ -1,3 +1,15 @@
+local AkaliNotif = loadstring(game:HttpGet("https://arscripts.space/nsNotify/Notifyns.lua"))();
+local Notify = AkaliNotif.Notify;
+
+wait(1);
+
+Notify({
+Description = "ADOPT ME | V0.1, SCRIPT IS LOADING IN PLEASE WAIT....";
+Title = "ADOPT ME | V0.1 ";
+Duration = 3;
+});
+
+
 local function bypassAC()
         loadstring(game:HttpGet("https://arscripts.space/adoptmetb/AdoptmeAC.lua"))()
 end
