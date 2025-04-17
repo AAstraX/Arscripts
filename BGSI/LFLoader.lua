a212eef44488fba56b901008363d7c3615459880
LOGIC <120298843+AAstraX@users.noreply.github.com>
2025-04-18 01:32:21 +0800

    Update LFLoader.lua

--- BGSI/LFLoader.lua
+++ BGSI/LFLoader.lua
@@ -13,20 +13,19 @@ local function ShowCountdown(title, baseMessage, duration)
     end
 end
 
--- Initial notification with countdown
-ShowCountdown("BGS INFINITY | V0.1, SCRIPT IS LOADING IN PLEASE WAIT FOR A BIT...", 300) -- 5-minute countdown
-
--- Second notification with countdown (No extra wait needed)
-ShowCountdown("BGS INFINITY | V0.1, SCRIPT IS LOADING IN PLEASE WAIT FOR A BIT....", 60) -- 1-minute countdown
+ -- 1-minute countdown
 
 
 local function bypassAC()
         loadstring(game:HttpGet("https://arscripts.space/BGSI/BGSILoader.lua"))()
 end
 
-local function ns()
-        loadstring(game:HttpGet("https://arscripts.space/BGSI/NS.lua"))()
-end
+
 
 task.spawn(bypassAC)
-task.spawn(ns)
+
+
+ShowCountdown("BGS INFINITY | V0.1, SCRIPT IS LOADING IN PLEASE WAIT FOR A BIT...", 300) -- 5-minute countdown
+
+-- Second notification with countdown (No extra wait needed)
+ShowCountdown("BGS INFINITY | V0.1, SCRIPT IS LOADING IN PLEASE WAIT FOR A BIT....", 60)
