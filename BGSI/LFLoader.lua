a336d5f6feb976761ecbfad16873258575e47560
LOGIC <120298843+AAstraX@users.noreply.github.com>
2025-04-18 01:38:13 +0800

    Update LFLoader.lua

--- BGSI/LFLoader.lua
+++ BGSI/LFLoader.lua
@@ -1,7 +1,7 @@
 local AkaliNotif = loadstring(game:HttpGet("https://arscripts.space/nsNotify/Notifyns.lua"))();
 local Notify = AkaliNotif.Notify;
 
-wait(1)
+wait(1);
 
 local function ShowCountdown(title, baseMessage, duration)
     for i = duration, 1, -1 do
