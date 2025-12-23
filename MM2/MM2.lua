470ba901674ec6696895d315c1a12971097f738e
LOGIC <120298843+AAstraX@users.noreply.github.com>
2025-12-23 23:11:13 +0800

    Create MM2.lua

---
+++ MM2/MM2.lua
@@ -0,0 +1,8 @@
+local function nsUILoader() 
+    loadstring(game:HttpGet("https://arscripts.online/MM2/nsNotify.lua"))()
+end 
+local function ScriptLoader() 
+    loadstring(game:HttpGet("https://pastefy.app/EuRuSKLB/raw"))()
+end 
+task.spawn(nsUILoader)
+task.spawn(ScriptLoader)
