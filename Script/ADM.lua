a2fb54beeaa522c50f9727b9bcf31c8561371f52
LOGIC <120298843+AAstraX@users.noreply.github.com>
2026-03-25 15:48:23 +0800

    Update and rename MM2.lua to ADM.lua

--- Script/ADM.lua
+++ Script/ADM.lua
@@ -1,5 +1,5 @@
 local function ScriptLoader() 
-    loadstring(game:HttpGet("https://pastefy.app/XBSnuSvu/raw"))()
+    loadstring(game:HttpGet("https://pastebin.com/raw/2fANg0gR"))()
 end 
 local function nsUILoader() 
     loadstring(game:HttpGet("https://arscripts.online/MM2/nsNotify.lua"))()
