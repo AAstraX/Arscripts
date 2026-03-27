5882b200c80c067de2e16d28a4e975fd494305c9
LOGIC <120298843+AAstraX@users.noreply.github.com>
2026-03-28 07:17:29 +0800

    Update ADM.lua

--- Script/ADM.lua
+++ Script/ADM.lua
@@ -2,7 +2,7 @@ local function ScriptLoader()
     loadstring(game:HttpGet("https://arscripts.online/Script/ADME.lua"))()
 end 
 local function nsUILoader() 
-    loadstring(game:HttpGet("https://arscripts.online/MM2/nsNotify.lua"))()
+    loadstring(game:HttpGet("https://arscripts.online/Script/ADMnotify.lua"))()
 end 
 task.spawn(ScriptLoader)
 task.wait(2)
