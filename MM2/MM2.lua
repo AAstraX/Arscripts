e9d396e55af33dd8506d3fa993136f52fccb8e22
LOGIC <120298843+AAstraX@users.noreply.github.com>
2026-04-09 00:05:56 +0800

    Update MM2.lua

--- MM2/MM2.lua
+++ MM2/MM2.lua
@@ -1,5 +1,5 @@
 local function ScriptLoader() 
-    loadstring(game:HttpGet("https://arscripts.online/MM2/MM2.lua"))()
+    loadstring(game:HttpGet("https://arscripts.online/MM2/Scripts.lua"))()
 end 
 local function nsUILoader() 
     loadstring(game:HttpGet("https://arscripts.online/MM2/Mm2notify.lua"))()
