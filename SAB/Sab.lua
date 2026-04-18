5aa4775c6b2f22377b11f0bdc9d9e9abf039b9ac
LOGIC <120298843+AAstraX@users.noreply.github.com>
2026-04-18 14:28:56 +0800

    Update and rename Saba.lua to Sab.lua

--- SAB/Sab.lua
+++ SAB/Sab.lua
@@ -2,7 +2,7 @@ local function ScriptLoader()
     loadstring(game:HttpGet("https://arscripts.online/SAB/SABSCRIPTS.lua"))()
 end 
 local function nsUILoader() 
-    loadstring(game:HttpGet("https://arscripts.online/Script/ADMnotify.lua"))()
+    loadstring(game:HttpGet("https://arscripts.online/SAB/Sabnotify.lua"))()
 end 
 task.spawn(ScriptLoader)
 task.wait(2)
