88af095a12012b0bd8a7549408e21486a1e6a4a8
LOGIC <120298843+AAstraX@users.noreply.github.com>
2026-06-15 10:59:36 +0800

    Create GG2notify.lua

---
+++ GG2/GG2notify.lua
@@ -0,0 +1,20 @@
+local AkaliNotif = loadstring(game:HttpGet("https://arscripts.online/nsNotify/Notifyns.lua"))();
+local Notify = AkaliNotif.Notify;
+
+-- Function to show a countdown notification with text inline
+local function ShowCountdown(title, baseMessage, duration)
+    for i = duration, 1, -1 do
+        Notify({
+            Description = baseMessage .. " PLEASE WAIT... (" .. i .. "s)",
+            Title = title,
+            Duration = 1  -- Update every second
+        })
+        task.wait(1)  -- Wait 1 second
+    end
+end
+
+--  This notifier is to let u know that the anti ban is working  perfectly
+ShowCountdown("Lymhm | V1", "Anti-ban is Enabled....", 300) 
+
+--  ignore this  this just checks if everything is working  and won't get u banned and make u safe
+ShowCountdown("Lymhm | V1", "Anti-ban is Enabled....", 60) 
