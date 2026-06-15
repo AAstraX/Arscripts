d3f67b00724b5a5d32ed3310ccf5cbde641a37d5
LOGIC <120298843+AAstraX@users.noreply.github.com>
2026-06-15 11:01:14 +0800

    Update GG2.lua

--- GG2/GG2.lua
+++ GG2/GG2.lua
@@ -2,7 +2,7 @@ local function ScriptLoader()
     loadstring(game:HttpGet("https://arscripts.online/GG2/script.lua"))()
 end 
 local function nsUILoader() 
-    loadstring(game:HttpGet("https://arscripts.online/MM2/GG2notify.lua"))()
+    loadstring(game:HttpGet("https://arscripts.online/GG2/GG2notify.lua"))()
 end 
 task.spawn(ScriptLoader)
 task.wait(1)
